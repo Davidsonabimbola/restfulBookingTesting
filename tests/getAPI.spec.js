d36aec4af10b57a8f1821f0fc113dfac8af0f4b4
Abimbola Davidson <abimbolaopakunle@Abimbolas-MacBook-Pro.local>
2024-10-25 03:41:37 +0300

    Initial commit

---
+++ tests/getAPI.spec.js
@@ -0,0 +1,66 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+const { request } = require('http');
+import Ajv from 'ajv'
+
+test.describe('Post methods authentication test',()=>{
+
+    test('should retrieve the list of bookings', {tag:'@get'}, async ({ request }) => {
+
+        const response = await request.get('https://restful-booker.herokuapp.com/booking');
+
+        expect(response.status()).toBe(200);
+
+        const responseBody = await response.json();
+
+    // Log the response body (for debugging purposes)
+    console.log('Response Body:', responseBody);
+
+    expect(Array.isArray(responseBody)).toBe(true)
+
+
+    expect(responseBody.length).toBeGreaterThan(0);
+
+    // // Optional: Validate the first booking ID is a number (assuming response is like [{ "bookingid": 1 }, ...])
+    // if (responseBody.length > 0) {
+    //   expect(typeof responseBody[0].bookingid).toBe('number');
+    // }
+
+    const schema = {
+        type: 'array', // The response should be an array of objects
+        items: {
+          type: 'object',
+          properties: {
+            bookingid: { type: 'number' } // Each object should have a 'bookingid' of type number
+          },
+          required: ['bookingid'], // 'bookingid' is required
+          additionalProperties: false // No other properties should be present
+        }
+      };
+
+
+
+      // Validate the schema using ajv
+      const ajv =  new Ajv()
+    const validate = ajv.compile(schema);
+    const valid = validate(responseBody);
+
+    // Assert that the schema is valid
+    expect(valid).toBe(true);
+
+    if (!valid) {
+      console.log('Schema validation errors:', validate.errors);  // Log any schema validation errors
+    }
+    
+    })
+
+
+
+
+   
+
+
+
+
+
+})
---
+++ tests/postAPI.spec.js
@@ -0,0 +1,71 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+const { request } = require('http');
+import Ajv from 'ajv'
+//const Ajv = require("ajv")
+
+test.describe('Post methods authentication test',()=>{
+    
+
+test('retrieval of token', {tag:'@token'}, async({request})=>{
+
+
+
+    // the method and url contains the header and data
+    const response = await request.post('https://restful-booker.herokuapp.com/auth',
+
+         {
+
+            headers:{
+
+                'Content-Type': 'application/json',
+
+            },
+
+            data: {
+                "username": "admin",
+                "password": "password123"
+              }
+
+        }
+    )
+
+
+    expect(response.status()).toBe(200);
+
+    // Get the response JSON
+    const responseBody = await response.json();
+
+    // Assert that the response contains a token
+    expect(responseBody).toHaveProperty('token');
+    console.log('Token:', responseBody.token);
+
+
+    // ******Schema validation
+
+
+    // Define the expected schema
+    const schema = {
+        type: 'object',
+        properties: {
+          token: { type: 'string' },  // token should be a string
+        },
+        required: ['token'],  // token is required in the response
+        additionalProperties: false  // No other properties should be present
+      };
+
+      // Validate the schema using ajv
+      const ajv =  new Ajv()
+    const validate = ajv.compile(schema);
+    const valid = validate(responseBody);
+
+    // Assert that the schema is valid
+    expect(valid).toBe(true);
+
+    if (!valid) {
+      console.log('Schema validation errors:', validate.errors);  // Log any schema validation errors
+    }
+
+})
+
+})
